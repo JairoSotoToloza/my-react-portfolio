d2a9bd71a4a285340de4686f5a5208995cedf54f
JZoto <jzoto@pop-os.localdomain>
2021-08-07 19:55:42 -0400

    Parrafos a español en about.js

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -9,29 +9,21 @@ export default class about extends Component {
                             <img className="s-about__pic" src="images/avatars/user-06.jpg" alt />
                         </div>
                         <div className="column large-9 tab-12 s-about__content">
-                            <h3>About Me</h3>
+                            <h3>A cerca de mi</h3>
                             <p>
-                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-                                ab illo inventore veritatis et quasi architecto beatae vitae dicta
-                                sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-                                aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-                                qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-                                dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-                                quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-                                aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-                                exercitationem ullam corporis suscipit laboriosam.
+                                Desarrollador de aplicaciones web, egresado de la carrera de Ingeniería en Informática
+                                de la universidad INACAP. 
                             </p>
                             <hr />
                             <div className="row s-about__content-bottom">
                                 <div className="column w-1000-stack">
-                                    <h3>Contact Details</h3>
+                                    <h3>Detalles de contacto</h3>
                                     <p>
-                                        Jonathan Doe <br />
-                                        1600 Amphitheatre Parkway <br />
-                                        Mountain View, CA 94043 US <br />
-                                        <a href="tel:+1975432345">+197 543 2345</a> <br />
-                                        <a href="mailto:#0">sayhello@ceevee.com</a>
+                                        Jairo Soto Toloza <br />
+                                        Puerto Grosse #584, San Bernardo <br />
+                                        Región Metropolitana de Chile <br />
+                                        <a href="tel:+56949167363">++569 4916 7363</a> <br />
+                                        <a href="mailto:#0">jairosototoloza@protonmail.com@ceevee.com</a>
                                     </p>
                                 </div>
                                 <div className="column w-1000-stack">
@@ -50,7 +42,7 @@ export default class about extends Component {
                                             <path d="M12 16L16 11 13 11 13 4 11 4 11 11 8 11z" />
                                             <path d="M20,18H4v-7H2v7c0,1.103,0.897,2,2,2h16c1.103,0,2-0.897,2-2v-7h-2V18z" />
                                         </svg>
-                                        Download CV
+                                        Descargar CV
                                     </a>
                                 </div>
                             </div>
