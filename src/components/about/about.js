f11f6dbcac3e0570331a6979ffcb2a46160f46ba
JZoto <jzoto@pop-os.localdomain>
2021-08-07 20:14:59 -0400

    Foto y datos actualizados

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -6,14 +6,12 @@ export default class about extends Component {
                 <section id="about" className="s-about target-section">
                     <div className="row">
                         <div className="column large-3 tab-12">
-                            <img className="s-about__pic" src="images/avatars/user-06.jpg" alt />
+                            <img className="s-about__pic" src="images/avatars/user-00.png" alt />
                         </div>
                         <div className="column large-9 tab-12 s-about__content">
-                            <h3>A cerca de mi</h3>
+                            <h3>A cerca de mí</h3>
                             <p>
-                                Desarrollador de aplicaciones web, egresado de la carrera de Ingeniería en Informática
-                                de la universidad INACAP. 
-                            </p>
+                               Ingeniero informático y desarrollador de aplicaciones web.</p>
                             <hr />
                             <div className="row s-about__content-bottom">
                                 <div className="column w-1000-stack">
@@ -21,9 +19,9 @@ export default class about extends Component {
                                     <p>
                                         Jairo Soto Toloza <br />
                                         Puerto Grosse #584, San Bernardo <br />
-                                        Región Metropolitana de Chile <br />
-                                        <a href="tel:+56949167363">++569 4916 7363</a> <br />
-                                        <a href="mailto:#0">jairosototoloza@protonmail.com@ceevee.com</a>
+                                        Región Metropolitana  <br />
+                                        <a href="tel:+56949167363">+569 4916 7363</a> <br />
+                                        <a href="mailto:#0">jairosototoloza@protonmail.com</a>
                                     </p>
                                 </div>
                                 <div className="column w-1000-stack">
@@ -42,7 +40,7 @@ export default class about extends Component {
                                             <path d="M12 16L16 11 13 11 13 4 11 4 11 11 8 11z" />
                                             <path d="M20,18H4v-7H2v7c0,1.103,0.897,2,2,2h16c1.103,0,2-0.897,2-2v-7h-2V18z" />
                                         </svg>
-                                        Descargar CV
+                                       Descargar CV 
                                     </a>
                                 </div>
                             </div>
